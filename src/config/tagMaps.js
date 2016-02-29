9f52ff697ced81fa4a13bae13540081d75bf75f2
David Burrows <david@designsuperbuild.com>
2016-02-29 18:19:24 +0000

    update package.json - add pre version number, repository, add missing babel bits, test command, dist commond for example

---
+++ src/config/tagMaps.js
@@ -0,0 +1,7 @@
+export const inlineTagMap = {
+  'BOLD': ['<strong>','</strong>'],
+  'ITALIC': ['<em>','</em>'],
+  'UNDERLINE': ['<u>','</u>'],
+  'CODE': ['<code>','</code>'],
+  'default': ['<span>','</span>']
+};
