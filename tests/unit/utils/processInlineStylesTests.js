2b1a4609f55e2c43fa84a1c50e795f9b1c8f8461
David Burrows <david@designsuperbuild.com>
2016-05-16 18:59:01 +0100

    fix incorrect tag nesting in inline styles + tests

--- tests/unit/utils/processInlineStylesTests.js
+++ tests/unit/utils/processInlineStylesTests.js
@@ -43,19 +43,93 @@ let overlappingInlineStyles2 = {
   "entityRanges": []
 };
 
+// <strong>aa<em>aaaaaa</strong>aa</em>
+let badlyNestedTags1 = {
+  "key": "d21l",
+  "text": "aaaaaaaaaa",
+  "type": "unstyled",
+  "depth": 0,
+  "inlineStyleRanges": [
+    {
+      "offset": 0,
+      "length": 8,
+      "style": "BOLD"
+    },
+    {
+      "offset": 2,
+      "length": 8,
+      "style": "ITALIC"
+    }
+  ],
+  "entityRanges": []
+};
+
+let badlyNestedTags2 = {
+  "key": "d21l",
+  "text": "Here's some text, it's useful but it's quote long this time",
+  "type": "unstyled",
+  "depth": 0,
+  "inlineStyleRanges": [
+    {
+      "offset": 2,
+      "length": 20,
+      "style": "BOLD"
+    },
+    {
+      "offset": 2,
+      "length": 2,
+      "style": "ITALIC"
+    },
+    {
+      "offset": 7,
+      "length": 30,
+      "style": "ITALIC"
+    },
+    {
+      "offset": 50,
+      "length": 4,
+      "style": "BOLD"
+    }
+  ],
+  "entityRanges": []
+};
+
+// In this case, the editor generates:
+
+// The <strong>quick brown fox <em>jumps over</strong> the lazy</em> dog.
+// Instead of:
+
+// The <strong>quick brown fox <em>jumps over</em></strong><em> the lazy</em> dog.
+
 
 describe('processInlineStylesAndEntities', function () {
 
-  it('should order blocks correctly', function() {
+  it('should order blocks correctly: test case 1', function() {
     let html = processInlineStylesAndEntities(inlineTagMap, {}, {}, overlappingInlineStyles);
 
     expect(html).to.equal('Here\'s some <strong>text, it\'s <em>useful</em></strong>');
   });
 
-  it('should order blocks correctly', function() {
+  it('should order blocks correctly: test case 2', function() {
     let html = processInlineStylesAndEntities(inlineTagMap, {}, {}, overlappingInlineStyles2);
 
     expect(html).to.equal(`Here's <em>some text, <strong>it's useful</strong></em>`);
+  });
+
+  it('should close/open nested inline tags properly: test case 1', function () {
+    let html = processInlineStylesAndEntities(inlineTagMap, {}, {}, badlyNestedTags1);
+
+    expect(html).to.equal(
+      `<strong>aa<em>aaaaaa</em></strong><em>aa</em>`);
+
+  });
+
+  it('should close/open tags properly: test case 2', function () {
+    let html = processInlineStylesAndEntities(inlineTagMap, {}, {}, badlyNestedTags2);
+
+    expect(html).to.equal(
+      `He<strong><em>re</em>'s <em>some text, it's</em></strong><em> useful but it'</em>s quote long <strong>this</strong> time`);
+
   });
 
 });
