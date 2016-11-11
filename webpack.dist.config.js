fb68153f02419199a22a6618d24eee056d176799
Julien Bouquillon <julien@revolunet.com>
2016-11-11 18:39:12 +0100

    prod build :
     - add `npm run dist` task and call it on `npm run release`
     - use webpack.dist.config.js to build to /dist/index.js without react/react-dom
     - use dist/index.js by default in package.json
     - update readme

---
+++ webpack.dist.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+  devtool: false,
+  entry: {
+    example: [
+      "./src/BasicHtmlEditor.js"
+    ]
+  },
+  output: {
+    path: './dist',
+    filename: "index.js"
+  },
+  externals: ['react', 'react-dom'],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: "style!css!postcss!sass"
+      },
+      {
+        test: /\.js$/,
+        exclude: [ /node_modules/],
+        loaders: ["babel-loader"]
+      }
+    ]
+  }
+};
