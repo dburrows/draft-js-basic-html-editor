126cd7a454b47384eb181ba97d9ec1e8f6b4a148
Dmitriy Grachev <dmitriy.grachev@erlyvideo.org>
2018-03-04 01:37:29 +0300

    fixed highliting remove link button when link is selected

--- src/components/EntityControls.js
+++ src/components/EntityControls.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import {  RichUtils } from 'draft-js';
 import StyleButton from './StyleButton';
+import { ENTITY_CONTROLS } from '../config/constants';
+
+const addLinkLabel = ENTITY_CONTROLS[0].label;
 
 export default function EntityControls(props) {
-  let { entityControls } = props;
-  var currentStyle = props.editorState.getCurrentInlineStyle();
+  const { entityControls, editorState } = props;
+  const isLink = RichUtils.currentBlockContainsLink(editorState);
   return (
     <div className="RichEditor-controls">
       {entityControls.map(type =>
         <StyleButton
           key={type.label}
-          active={currentStyle.has(type.style)}
+          active={isLink && type.label !== addLinkLabel}
           label={type.label}
           onToggle={type. action}
         />
